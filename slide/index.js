c3323a5204266c1ca4075ed351ecd6831615bf2d
Sebastián Lara Menares <slaramen@gmail.com>
2013-06-12 14:30:25 -0400

    Add automatic slide integration. Closes #1

--- slide/index.js
+++ slide/index.js
@@ -1,6 +1,8 @@
 'use strict';
 var util = require('util');
 var yeoman = require('yeoman-generator');
+var fs = require('fs');
+var path = require('path');
 
 var SlideGenerator = module.exports = function SlideGenerator(args, options, config) {
   // By calling `NamedBase` here, we get the argument to the subgenerator call
@@ -13,6 +15,12 @@ var SlideGenerator = module.exports = function SlideGenerator(args, options, con
 util.inherits(SlideGenerator, yeoman.generators.NamedBase);
 
 SlideGenerator.prototype.files = function files() {
-  var filename = 'slides/' + this._.slugify(this.name) + '.md';
-  this.template('slide.md', filename);
+  var filename = this._.slugify(this.name) + '.md';
+  var fullfilename = 'slides/' + filename;
+  this.template('slide.md', fullfilename);
+  var appPath = process.cwd();
+  var fullPath = path.join(appPath, '/slides/list.json');
+  var list = require(fullPath);
+  list.push(filename);
+  fs.writeFileSync(fullPath, JSON.stringify(list))
 };
